14f45c203a949b1a855a80932fc9aff6b3f888b9
Elias Hansen <pe_lias@msn.com>
2017-10-22 18:04:55 +0200

    Added ThemeProvider to App

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './Components/App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/App'
+import registerServiceWorker from './registerServiceWorker'
+import './styles/app.css'
+import theme from './styles/theme'
+import { ThemeProvider } from 'styled-components'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>,
+  document.getElementById('root')
+);
+
 registerServiceWorker();
