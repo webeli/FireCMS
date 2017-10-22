ca19a1a7ff3a3a10a8644c094c06232dab558759
Elias Hansen <pe_lias@msn.com>
2017-10-22 18:54:45 +0200

    Using sidebar width from theme

--- src/components/shared/Sidebar.js
+++ src/components/shared/Sidebar.js
@@ -2,7 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Sidebar = styled.div`
-  width: ${props => `calc(30% - (${props.theme.spacing.medium} * 2))`};
+  width: ${props => `calc(${props.theme.sidebar.width} - (${props.theme.spacing.medium} * 2))`};
   min-height: ${props => `calc(100vh - ${props.theme.header.height} - (${props.theme.spacing.medium} * 4))`};
   padding: ${props => props.theme.spacing.medium};
   box-shadow: inset 0px 0px 1px #CCC;
