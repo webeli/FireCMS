c8c2617f238930823ff8adfe39358d76edbea503
Elias Hansen <pe_lias@msn.com>
2017-10-22 18:54:35 +0200

    Using content width from theme

--- src/components/shared/Content.js
+++ src/components/shared/Content.js
@@ -2,7 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Content = styled.div`
-  width: ${props => `calc(70% - (${props.theme.spacing.medium} * 2))`};
+  width: ${props => `calc(${props.theme.content.width} - (${props.theme.spacing.medium} * 2))`};
   min-height: ${props => `calc(100vh - ${props.theme.header.height} - (${props.theme.spacing.medium} * 4))`};
   padding: ${props => props.theme.spacing.medium};
   box-shadow: inset 0px 0px 1px #CCC;
