341165a965b69e65b957261ebd1202552f4814a9
Elias Hansen <pe_lias@msn.com>
2017-10-22 18:03:53 +0200

    Created theme.js

---
+++ src/styles/theme.js
@@ -0,0 +1,18 @@
+export default {
+    header: {
+        height: '40px',
+        width: '100%',
+    },
+    sidebar: {
+        width: '40px',
+    },
+    colors: {
+        primary: '#FFF',
+        secondary: '#000',
+    },
+    spacing: {
+        small: '5px',
+        medium: '10px',
+        large: '15px',
+    }
+};
