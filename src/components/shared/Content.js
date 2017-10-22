8e45f1d8851933146360f34b187a94262d85212d
Elias Hansen <pe_lias@msn.com>
2017-10-22 18:48:56 +0200

    Calculating padding and min-height on Content and Sidebar

--- src/components/shared/Content.js
+++ src/components/shared/Content.js
@@ -2,9 +2,10 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Content = styled.div`
-  width: 60%;
-  min-height: ${props => `calc(100vh - ${props.theme.header.height})`};
+  width: ${props => `calc(70% - (${props.theme.spacing.medium} * 2))`};
+  min-height: ${props => `calc(100vh - ${props.theme.header.height} - (${props.theme.spacing.medium} * 4))`};
   padding: ${props => props.theme.spacing.medium};
+  box-shadow: inset 0px 0px 1px #CCC;
 `
 
 export default (props) => (
--- src/components/shared/Sidebar.js
+++ src/components/shared/Sidebar.js
@@ -2,9 +2,10 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Sidebar = styled.div`
-  width: 40%;
-  min-height: ${props => `calc(100vh - ${props.theme.header.height})`};
+  width: ${props => `calc(30% - (${props.theme.spacing.medium} * 2))`};
+  min-height: ${props => `calc(100vh - ${props.theme.header.height} - (${props.theme.spacing.medium} * 4))`};
   padding: ${props => props.theme.spacing.medium};
+  box-shadow: inset 0px 0px 1px #CCC;
 `
 
 export default (props) => (
