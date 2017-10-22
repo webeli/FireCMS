558d5565c6eda1feb3c144ff12ede9c003b6fff2
Elias Hansen <pe_lias@msn.com>
2017-10-22 21:41:37 +0200

    Created store

---
+++ src/store.js
@@ -0,0 +1,8 @@
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import reducer from './reducers';
+
+export default createStore(reducer, composeWithDevTools(
+  applyMiddleware(thunk)
+));
