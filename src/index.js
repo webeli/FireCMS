9089cdc3b9a490adfd15d9790c7aef53fb2f38d9
Elias Hansen <pe_lias@msn.com>
2017-10-22 21:41:30 +0200

    Added Provider

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
+import './styles/app.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
 import registerServiceWorker from './registerServiceWorker'
-import './styles/app.css'
+
 import theme from './styles/theme'
 import { ThemeProvider } from 'styled-components'
 
+import store from './store';
+import { Provider } from 'react-redux';
+
 ReactDOM.render(
   <ThemeProvider theme={theme}>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </ThemeProvider>,
   document.getElementById('root')
 );
