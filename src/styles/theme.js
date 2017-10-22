c419991282a66e830a5fc7e025bb1e7a69f67a18
Elias Hansen <pe_lias@msn.com>
2017-10-22 18:55:29 +0200

    Updated theme with sidebar and content, fixed indents

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,18 +1,21 @@
 export default {
-    header: {
-        height: '40px',
-        width: '100%',
-    },
-    sidebar: {
-        width: '40px',
-    },
-    colors: {
-        primary: '#FFF',
-        secondary: '#000',
-    },
-    spacing: {
-        small: '5px',
-        medium: '10px',
-        large: '15px',
-    }
+  header: {
+    height: '40px',
+    width: '100%',
+  },
+  sidebar: {
+    width: '25%',
+  },
+  content: {
+    width: '75%',
+  },
+  colors: {
+    primary: '#FFF',
+    secondary: '#000',
+  },
+  spacing: {
+    small: '5px',
+    medium: '10px',
+    large: '15px',
+  }
 };
