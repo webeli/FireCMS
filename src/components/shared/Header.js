96357f5048e846ef536edf60a07e2913cb77dcba
Elias Hansen <pe_lias@msn.com>
2017-10-22 18:04:04 +0200

    Created Header.js

---
+++ src/components/shared/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Header = styled.div`
+	display: flex;
+	align-items: center;
+	width: 100%;
+	height: ${props => props.theme.header.height};
+	padding: ${props => props.theme.spacing.medium};
+	background: #000;
+	color: #FFF;
+`
+
+export default (props) => (
+	<Header>
+		<span>Header</span>
+  </Header>
+)
